e7bbe05b41e72b863335b44ba56de93147247446
simon <sbracegirdle@gmail.com>
2013-08-04 13:25:44 +0800

    MongoDB dev

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 //require('mootools');
+require('mootools');
+require('mootools-more');
 var express = require('express');
-var actions = require('./actions');
+var MarkyDB = require('./db').MarkyDB;
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.configure(function() {
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname + '/public'));
+});
 
 //app.get('/json/')
 
-app.get('/test/', actions.test);
-app.get('/save/', actions.save);
+var db = new MarkyDB({ // TODO Configurable
+	host: 'localhost',
+	port: 27017, 
+	name: 'marky'
+});
+
+app.get('/tree/get', db.getTree);
+app.get('/download', db.dump);
+app.get('/folders/get', db.getFolders);
+app.get('/note/get', db.getNote);
+app.get('/note/add', db.addNote);
+app.get('/note/delete', db.deleteNote);
+app.get('/folder/add', db.addFolder);
+app.get('/colour/save', db.saveColour);
+app.get('/content/save', db.saveContent);
+app.get('/name/save', db.saveName);
+app.get('/move', db.move);
 
 //app.post('/json/', authFunc,)
 
-app.listen(3000);
+//resp.redirect() ??
+
+app.listen(3000); // TODO Configurable
+console.log('Now listening on port 3000...');
