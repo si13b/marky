81eb3a3179f3b60ffd35f1e2fd73e7497f97ba79
simon <sbracegirdle@gmail.com>
2013-10-29 08:03:20 +0800

    Further test development

--- public/js/tests.js
+++ public/js/tests.js
@@ -21,7 +21,7 @@
 					F('input[name="password"]').visible('P/W field visible').type('admin123');
 					F('input[type="Submit"]').visible('Login button visible').click();
 			
-					ok( 1 == "1", "Passed!" );
+					ok( 1 == "1", 'Basic assertion' );
 					F('nav').size(1, function() {
 						F('nav').visible('Find navigation panel');
 						F('nav > h1').visible('Marky title should be visible!');
@@ -51,11 +51,19 @@
 			.type('[delete][delete][delete][delete][delete][delete][delete][delete][delete][delete][delete][delete][delete][delete][delete]');
 	}
 	
-	test('Test note basics', function() {
+	test('Note creation and deletion', function() {
 		var elNew1 = null,
+			elNew2 = null,
 			new1ID = null,
+			new2ID = null,
 			count = null,
 			elAce = null;
+			
+		checkLogin(function() {
+			count = F('ul.tree li').size();
+			F('.opts > div[title="New note"]').click();
+			F('ul.tree li').size(count + 1, 1000, createFirstNote);
+		});
 			
 		function createFirstNote() {
 			elNew1 = F('ul.tree > li:last-child');
@@ -70,6 +78,7 @@
 				elAce.click().click().then(editFirstNote);
 			});
 		}
+		
 		function editFirstNote() {
 			//$(elAce).text('# This is a title\nAnd this is some text');
 			var noteOneText = '# This is a title\nAnd this is some text';
@@ -83,19 +92,20 @@
 		function editSecondNote() {
 			F('.opts > div[title="New note"]').click();
 			F('ul.tree li').size(count + 2, 1000, function() {
-				var elNew2 = F('ul.tree > li:last-child');
+				elNew2 = F('ul.tree > li:last-child');
 				equal(elNew2.text().trim(), 'New note', 'New note has been created');
 				elNew2.click();
+				new2ID = elNew2.attr('data-id');
 				
 				F.wait(200, function() {
 					equal(elAce.text().trim().length, 0, 'ACE editor should now be empty');
 					setACE('New note two');
-					returnToFirstNote();
+					checkFirstNoteLabel();
 				});
 			});
 		}
 		
-		function returnToFirstNote() {
+		function checkFirstNoteLabel() {
 			elNew1 = F('li[data-id="' + new1ID + '"]');
 			elNew1.click();
 			F.wait(200, function(){
@@ -112,13 +122,64 @@
 				F('textarea.ace_text-input').click();
 				F('.editor.selected > .title > input').val(newLabelText);
 				ok(elNew1.text().contains(newLabelText), 'Menu item for note contains new label');
+				F.wait(200, deleteNotes);
 			});
 		}
 		
+		function deleteNotes() {
+			elNew1.click();
+			F.wait(200, function() {
+				F('.editor > .toolbar div[title="Delete"]').click();
+				F('li[data-id="' + new1ID + '"]').missing(function() {
+					elNew2 = F('ul.tree li[data-id="' + new2ID + '"]');
+					elNew2.click();
+					F.wait(200, function() {
+						F('.editor > .toolbar div[title="Delete"]').click();
+						F('ul.tree li[data-id="' + new2ID + '"]').missing('The second note has now been removed');
+					});
+				}, 'The first note has now been removed');
+			});
+		}
+	});
+	
+	test('Folder creation and deletion', function() {
+		var noteIDs = [],
+			folderID = null,
+			elFolder = null;
+			
 		checkLogin(function() {
 			count = F('ul.tree li').size();
 			F('.opts > div[title="New note"]').click();
-			F('ul.tree li').size(count + 1, 1000, createFirstNote);
+			F('.opts > div[title="New note"]').click();
+			F('.opts > div[title="New note"]').click();
+			F.wait(200, function() {
+				for (var i = 0; i < 3; i++) noteIDs.push(F('ul.tree > li:nth-child(' + (count + i) + ')').attr('data-id'));
+				
+				renameNote(1);
+			});
 		});
+		
+		function renameNote(number) {
+			F('li[data-id="' + noteIDs[number - 1] + '"]').click();
+			F.wait(200, function() {
+				massDeleteAction(F('.editor.selected > .title > input')).click().type('Note number ' + number + '[enter]');
+				setACE('# A note!\n* Note number ' + number);
+				F.wait(200, function() {
+					if (number >= 3) createFolder();
+					else renameNote(number + 1);
+				});
+			});
+			
+		}
+		
+		function createFolder() {
+			F('.opts > div[title="New folder"]').click();
+			F.wait(200, function() {
+				folderID = F('ul.tree > li.folder:last-child').attr('data-id');
+				elFolder = F('ul.tree > li.folder[data-id="' + folderID + '"]');
+				elFolder.click().hasClass('expanded', true);
+				
+			});
+		}
 	});
 })();
